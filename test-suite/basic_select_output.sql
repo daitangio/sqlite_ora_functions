a5e2c1a0a032b3b01c462ba43f7165992d76199a
Giovanni Giorgi <giovanni.giorgi@nttdata.com>
2019-08-20 11:00:45 +0200

    Added .dump

--- test-suite/basic_select_output.sql
+++ test-suite/basic_select_output.sql
@@ -2,5 +2,11 @@ select 'Test' as Column1, 'test2' as column2 ;
 
 select null as C1, 'value' as C2, null as C3_null;
 
+-- Basic dump test
+create table dump_me (a text);
+insert into dump_me  values ('dumped');
+
+.dump
+
 .exit
 
