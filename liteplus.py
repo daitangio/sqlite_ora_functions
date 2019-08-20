8a320f9c00882cf4571b9a5ee749815224a93649
Giovanni Giorgi <giovanni.giorgi@nttdata.com>
2019-08-20 11:03:36 +0200

    Reformatted with python black

--- liteplus.py
+++ liteplus.py
@@ -12,30 +12,32 @@ import sqlite3
 import functools
 
 ######################################
-# Low level support 
+# Low level support
 ######################################
 """
  Print a cursor result
 """
+
+
 def printResult(cx):
-    currentRecord=cx.fetchone()
-    if currentRecord!=None:
-        sys.stdout.write('\n')
-        colnames=''+('|'.join(currentRecord.keys()))+'|'
-        sys.stdout.write(colnames+'\n')
-        for x in range(1,len(colnames)+1):
-            sys.stdout.write('-')
-        sys.stdout.write('\n')
+    currentRecord = cx.fetchone()
+    if currentRecord != None:
+        sys.stdout.write("\n")
+        colnames = "" + ("|".join(currentRecord.keys())) + "|"
+        sys.stdout.write(colnames + "\n")
+        for x in range(1, len(colnames) + 1):
+            sys.stdout.write("-")
+        sys.stdout.write("\n")
         while currentRecord != None:
-            row_str=str(tuple(currentRecord))
+            row_str = str(tuple(currentRecord))
             for elem in tuple(currentRecord):
-                if elem!=None:
+                if elem != None:
                     sys.stdout.write(str(elem))
                 else:
                     sys.stdout.write("null")
-                sys.stdout.write('|')
-            sys.stdout.write('\n')
-            currentRecord=cx.fetchone()    
+                sys.stdout.write("|")
+            sys.stdout.write("\n")
+            currentRecord = cx.fetchone()
 
 
 ####################################
@@ -53,16 +55,25 @@ class handle_exception(object):
         The __call__ method is not called until the
         decorated function is called.
         """
-        try:            
+        try:
             return self.f(*args)
         except:
-            print("[LITE-ERROR] Detected Exception thrown by %s: %s" % (self.f.__name__,sys.exc_info()[0]))
+            print(
+                "[LITE-ERROR] Detected Exception thrown by %s: %s"
+                % (self.f.__name__, sys.exc_info()[0])
+            )
             raise
+
+
 ###################################
 """Yield successive n-sized chunks from l."""
+
+
 def chunks(l, n):
     for i in range(0, len(l), n):
-        yield l[i:i + n]
+        yield l[i : i + n]
+
+
 ###################################
 #################### FUNCTIONS STARTS HERWE ##############################
 """
@@ -74,27 +85,30 @@ Ref https://docs.oracle.com/cd/B19306_01/server.102/b14200/functions040.htm
 
 The implementation required some work
 """
+
+
 @handle_exception
 def oracle_decode(*arg):
     # Now default is present if the size is
-    if(len(arg) < 3):
-        raise Exception('Provide at least 3 input to decode')
-    defaultPresent = (len(arg) %2) == 0
+    if len(arg) < 3:
+        raise Exception("Provide at least 3 input to decode")
+    defaultPresent = (len(arg) % 2) == 0
     if defaultPresent:
-        defaultValue=arg[-1]
-        varlist=arg[1:-1]
+        defaultValue = arg[-1]
+        varlist = arg[1:-1]
     else:
-        defaultValue=None
-        varlist=arg[1:]
-    expr=arg[0]
-    for pairs in list(chunks(varlist,2)):
-        search,result=pairs
-        #print(" %s -> %s " %(search,result))
-        if expr==search:
+        defaultValue = None
+        varlist = arg[1:]
+    expr = arg[0]
+    for pairs in list(chunks(varlist, 2)):
+        search, result = pairs
+        # print(" %s -> %s " %(search,result))
+        if expr == search:
             return result
     return defaultValue
 
-#print(oracle_decode("zz",1,2,2,4,'default'))
+
+# print(oracle_decode("zz",1,2,2,4,'default'))
 
 """
 https://docs.oracle.com/cd/B19306_01/server.102/b14200/functions183.htm
@@ -108,12 +122,15 @@ Also, the supported format is the C standard (1989 version)
 
 The Function is cached for performance reason
 """
+
+
 @handle_exception
 @functools.lru_cache(maxsize=64, typed=True)
 def oracle_to_date(string2convert, fmt, nlsparam=None):
-    dobj=datetime.datetime.strptime(string2convert, fmt)
+    dobj = datetime.datetime.strptime(string2convert, fmt)
     # Return a nice Sqlite date string
-    return dobj.isoformat(sep=' ',timespec='seconds')
+    return dobj.isoformat(sep=" ", timespec="seconds")
+
 
 """ 
 FIXME TO IMPLEMENT
@@ -124,9 +141,12 @@ TO_CHAR (datetime) converts a datetime or interval value of DATE, TIMESTAMP, TIM
 
 The 'nlsparam' argument specifies the language in which month and day names and abbreviations are returned.
 """
+
+
 @handle_exception
-def oracle_to_char(input,fmt=None,nlsparam=None):
+def oracle_to_char(input, fmt=None, nlsparam=None):
     pass
+
 
 """
 ORACLE:
@@ -163,29 +183,41 @@ A period (.) does not match the newline character.
 The source string is treated as a single line.
 
 """
-@handle_exception
-def oracle_regexp_replace(source,pattern,replace_string,position=1,occurence=0,match_parameter=None):
-    pythonFlags=translate_oracle_match_parameters(match_parameter)
-    if position == 1:
-        return re.sub(pattern,replace_string,source,count=occurence,flags=pythonFlags)
-    else:
-        return source[0:(position-1)] +  re.sub(pattern,replace_string,source[(position-1):],count=occurence,flags=pythonFlags)
 
+
+@handle_exception
+def oracle_regexp_replace(
+    source, pattern, replace_string, position=1, occurence=0, match_parameter=None
+):
+    pythonFlags = translate_oracle_match_parameters(match_parameter)
+    if position == 1:
+        return re.sub(
+            pattern, replace_string, source, count=occurence, flags=pythonFlags
+        )
+    else:
+        return source[0 : (position - 1)] + re.sub(
+            pattern,
+            replace_string,
+            source[(position - 1) :],
+            count=occurence,
+            flags=pythonFlags,
+        )
 
 
 def translate_oracle_match_parameters(match_parameter):
-    pythonFlags=0
+    pythonFlags = 0
     if match_parameter:
-        if 'i' in match_parameter:
-            pythonFlags |=re.IGNORECASE
+        if "i" in match_parameter:
+            pythonFlags |= re.IGNORECASE
             # 'c' is the default
-        if 'n' in match_parameter:
-            pythonFlags |=re.DOTALL
-        if 'm' in match_parameter:
-            pythonFlags |=re.MULTILINE
-        if 'x' in match_parameter:
+        if "n" in match_parameter:
+            pythonFlags |= re.DOTALL
+        if "m" in match_parameter:
+            pythonFlags |= re.MULTILINE
+        if "x" in match_parameter:
             raise Exception("x flag is unimplemented")
     return pythonFlags
+
 
 """
 Ref https://docs.oracle.com/database/121/SQLRF/conditions007.htm#SQLRF00501
@@ -203,10 +235,13 @@ pattern is the regular expression. It is usually a text literal and can be of an
 match_parameter is a text literal that lets you change the default matching behavior of the function.
 
 """
+
+
 @handle_exception
-def oracle_regexp_like(source,pattern,match_parameter=None):
-    pythonFlags=translate_oracle_match_parameters(match_parameter)
-    return 1 if re.search(pattern,source,flags=pythonFlags) else 0
+def oracle_regexp_like(source, pattern, match_parameter=None):
+    pythonFlags = translate_oracle_match_parameters(match_parameter)
+    return 1 if re.search(pattern, source, flags=pythonFlags) else 0
+
 
 """
 Ref https://docs.oracle.com/database/121/SQLRF/functions131.htm#SQLRF00684
@@ -219,8 +254,10 @@ The arguments expr1 and expr2 can have any data type. If their data types are di
 
 
 """
+
+
 @handle_exception
-def oracle_nvl(expr1,expr2):
+def oracle_nvl(expr1, expr2):
     if expr1:
         return expr1
     else:
@@ -231,8 +268,10 @@ def oracle_nvl(expr1,expr2):
 NVL2 lets you determine the value returned by a query based on whether a specified expression is null or not null. 
 If expr1 is not null, then NVL2 returns expr2. If expr1 is null, then NVL2 returns expr3.
 """
+
+
 @handle_exception
-def oracle_nvl2(expr1,expr2,expr3):
+def oracle_nvl2(expr1, expr2, expr3):
     if expr1:
         return expr2
     else:
@@ -253,33 +292,38 @@ https://oracle-base.com/articles/10g/utl_mail-send-email-from-the-oracle-databas
 https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.create_aggregate
 
 """
+
+
 @handle_exception
-def getImapMailboxHeaders(server,user,password,path,searchSpec=None):
-    import imaplib, getpass,email,email.header
+def getImapMailboxHeaders(server, user, password, path, searchSpec=None):
+    import imaplib, getpass, email, email.header
 
     M = imaplib.IMAP4_SSL(server)
 
     try:
-        rv, data = M.login(user, password)        
+        rv, data = M.login(user, password)
     except imaplib.IMAP4.error as err:
         raise err
     rv, mailboxes = M.list()
-    if rv != 'OK':
-        raise Exception("Cannot list mailboxes:"+rv)
+    if rv != "OK":
+        raise Exception("Cannot list mailboxes:" + rv)
     rv, data = M.select(path)
-    # TODO CONTINUE    
+    # TODO CONTINUE
     raise Exception("Still not implemented")
 
 
 """
  Enable access to file system: list files
 """
+
+
 @handle_exception
 def filesystem_fs(path_str, glob_stuff="*", sep=","):
-    from pathlib import Path    
-    l=[]
+    from pathlib import Path
+
+    l = []
     for f in Path(path_str).glob(glob_stuff):
-        l.append(f.name)    
+        l.append(f.name)
     return sep.join(l)
 
 
@@ -289,120 +333,130 @@ def filesystem_fs(path_str, glob_stuff="*", sep=","):
 """
 Assert function is used for unit testing
 """
-@handle_exception
-def assert_equals(expected,value,msg=""):
-    if value!=expected:
-        if msg !="":
-            return ( ("Test FAILED: Expected: %s instead of %s TestCase:"+msg) % (expected,value))
-        else:
-            return ( "Test FAILED: Expected: %s instead of %s" % (expected,value))
-    else:
-        return 'ok'
 
-    
+
+@handle_exception
+def assert_equals(expected, value, msg=""):
+    if value != expected:
+        if msg != "":
+            return ("Test FAILED: Expected: %s instead of %s TestCase:" + msg) % (
+                expected,
+                value,
+            )
+        else:
+            return "Test FAILED: Expected: %s instead of %s" % (expected, value)
+    else:
+        return "ok"
+
 
 def showHelp():
     print("HELP:")
     print("Lite*Plus: your full stop to SQLite3 compatibility layer")
     print("Super charge your script with a bunch of ad-hoc python functions")
-    print(""" Commands:
+    print(
+        """ Commands:
     .exit                        Exit from liteplus
     .help                        This help message
     .print  Message              
     .dump                        Execute a dump of the current database
-    """)
-
-
+    """
+    )
 
 
 # GLOBAL
-registeredFunctions=0
+registeredFunctions = 0
+
+
 def main(argv=sys.argv):
 
-    databaseName=argv[1]
+    databaseName = argv[1]
     con = sqlite3.connect(databaseName)
-    
+
     sqlite3.enable_callback_tracebacks(True)
-    
-    def register(fname, args,func):
+
+    def register(fname, args, func):
         global registeredFunctions
-        con.create_function(fname,args,func)
-        #print("R*%i -> %s" % ( args,fname))
-        registeredFunctions = registeredFunctions+1
-        
-    register('assert_equals',2,assert_equals)
-    register('assert_equals',3,assert_equals)
-    
-    register('regexp_replace', 3, oracle_regexp_replace)
-    register('regexp_replace', 4, oracle_regexp_replace)
-    register('regexp_replace', 5, oracle_regexp_replace)
-    register('regexp_replace', 6, oracle_regexp_replace)
-    
+        con.create_function(fname, args, func)
+        # print("R*%i -> %s" % ( args,fname))
+        registeredFunctions = registeredFunctions + 1
 
-    register('regexp_like',2,oracle_regexp_like)
-    register('regexp_like',3,oracle_regexp_like)
+    register("assert_equals", 2, assert_equals)
+    register("assert_equals", 3, assert_equals)
 
-    register('nvl',2,oracle_nvl)    
-    register('nvl2',3,oracle_nvl2)
+    register("regexp_replace", 3, oracle_regexp_replace)
+    register("regexp_replace", 4, oracle_regexp_replace)
+    register("regexp_replace", 5, oracle_regexp_replace)
+    register("regexp_replace", 6, oracle_regexp_replace)
 
-    register('to_date',2,oracle_to_date)
-    register('to_date',3,oracle_to_date)
+    register("regexp_like", 2, oracle_regexp_like)
+    register("regexp_like", 3, oracle_regexp_like)
+
+    register("nvl", 2, oracle_nvl)
+    register("nvl2", 3, oracle_nvl2)
+
+    register("to_date", 2, oracle_to_date)
+    register("to_date", 3, oracle_to_date)
 
     # Decode accepts variable data
-    register('decode',-1,oracle_decode)
+    register("decode", -1, oracle_decode)
 
-    register("fs",1,filesystem_fs)
-    register("fs",2,filesystem_fs)
-    
+    register("fs", 1, filesystem_fs)
+    register("fs", 2, filesystem_fs)
+
     # Push some math functions
     import math
-    for f in [math.sin,math.cos,math.tan,math.ceil,math.floor]:
-         register(f.__name__,1,f)
+
+    for f in [math.sin, math.cos, math.tan, math.ceil, math.floor]:
+        register(f.__name__, 1, f)
 
     con.execute("PRAGMA foreign_keys = ON")
-    #con.execute("pragma journal_mode=wal")
-    LITE_PLUS_VERSION="SQLite*Plus:%s on %s Registered Functions: %i" % (sqlite3.sqlite_version, databaseName, registeredFunctions)
+    # con.execute("pragma journal_mode=wal")
+    LITE_PLUS_VERSION = "SQLite*Plus:%s on %s Registered Functions: %i" % (
+        sqlite3.sqlite_version,
+        databaseName,
+        registeredFunctions,
+    )
     print(LITE_PLUS_VERSION)
-    #print(databaseName+">")
+    # print(databaseName+">")
     # REPL cycle
-    buffer=""
-    commandExecuted=0
+    buffer = ""
+    commandExecuted = 0
     con.row_factory = sqlite3.Row
     while True:
-        l=sys.stdin.readline()
+        l = sys.stdin.readline()
         buffer += l
-        if ';' in l:
+        if ";" in l:
             # See https://github.com/jonathanslenders/python-prompt-toolkit/blob/master/examples/tutorial/sqlite-cli.py
             try:
-                cx=con.cursor()
+                cx = con.cursor()
                 cx.execute(buffer)
                 printResult(cx)
             except Exception as e:
                 print("[LITE-ERROR] On the following chunk:")
-                print("\t%s" %(buffer))
-                print("[LITE-ERROR] \t"+repr(e))
-            commandExecuted +=1
-            buffer=""
-            sys.stdout.write("\n%i-%i>" %(commandExecuted, con.total_changes))
+                print("\t%s" % (buffer))
+                print("[LITE-ERROR] \t" + repr(e))
+            commandExecuted += 1
+            buffer = ""
+            sys.stdout.write("\n%i-%i>" % (commandExecuted, con.total_changes))
         else:
-            liteCmd=l.strip()
-            if liteCmd=='.exit':
+            liteCmd = l.strip()
+            if liteCmd == ".exit":
                 print("LITE PLUS EXITING...")
                 break
-            elif liteCmd=='.dump':
-                sys.stdout.write("\n-- Dump generated by "+str(LITE_PLUS_VERSION)+"\n")
+            elif liteCmd == ".dump":
+                sys.stdout.write(
+                    "\n-- Dump generated by " + str(LITE_PLUS_VERSION) + "\n"
+                )
                 for line in con.iterdump():
                     sys.stdout.write(line)
-                    sys.stdout.write("\n")                
-            elif liteCmd.startswith('.print '):
-                print("\n%s\n"%(liteCmd[6:]))
-                buffer=""                        
-            elif liteCmd=='.help':
+                    sys.stdout.write("\n")
+            elif liteCmd.startswith(".print "):
+                print("\n%s\n" % (liteCmd[6:]))
+                buffer = ""
+            elif liteCmd == ".help":
                 showHelp()
-                buffer=""
+                buffer = ""
 
 
-
-        
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
